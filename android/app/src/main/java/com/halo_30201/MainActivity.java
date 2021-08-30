4f4f8682420b300012c49565dfbf791e0ca713bd
Crowdbotics <team@crowdbotics.com>
2021-08-30 16:40:22 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/halo_30201/MainActivity.java
@@ -0,0 +1,15 @@
+package com.halo_30201;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "halo_30201";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "halo_30201Identifier",
+  url: "https://halo_30201Identifier.botics.co",
+  api: "https://halo_30201Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
